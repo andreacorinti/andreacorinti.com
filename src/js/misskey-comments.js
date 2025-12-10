c4a066100ccf5b8a1be7102850ad76fc263875d4
Andrea Corinti <andreacorinti@outlook.it>
2025-12-10 11:18:00 +0100

    aggiorno codice

--- src/js/misskey-comments.js
+++ src/js/misskey-comments.js
@@ -1,21 +1,59 @@
 const styles = `
 :root {
-  --font-color: #eceef7;
+  --font-color: #e0e0e0; /* Testo chiaro */
   --font-size: 1.0rem;
 
+  --main-background: #1e1e1e; /* Sfondo principale scuro */
   --block-border-width: 1px;
-  --block-border-radius: 3px;
-  --block-border-color: gray;
-  --block-background-color: #121212;
+  --block-border-radius: 5px;
+  --block-border-color: #333333; /* Bordo scuro leggero */
+  --block-background-color: #282828; /* Sfondo dei commenti più chiaro del main */
 
-  --comment-indent: 20px;
+  --comment-indent: 40px;
 }
 
-#mastodon-comments-list {
+/* Applica lo sfondo principale scuro all'elemento contenitore */
+misskey-comments {
+  font-size: var(--font-size);
+  color: var(--font-color);
+  background-color: var(--main-background);
+  padding: 15px; /* Aggiungi un po' di padding per staccare dal body */
+}
+
+p {
+  margin: 0 0 1rem 0;
+  color: var(--font-color);
+}
+
+a {
+  color: #6a9fb5; /* Link leggibili */
+}
+
+#misskey-stats {
+  text-align: center;
+  font-size: calc(var(--font-size) * 1.5);
+}
+
+#misskey-title {
+  font-size: calc(var(--font-size) * 1.5);
+  font-weight: bold;
+  color: #f0f0f0; /* Titoli molto chiari */
+}
+
+#misskey-comments-list {
   margin: 0 auto;
+  padding: 0;
 }
 
-.mastodon-comment {
+#misskey-comments-list ul {
+  padding-left: var(--comment-indent);
+}
+
+#misskey-comments-list li {
+  list-style: none;
+}
+
+.misskey-comment {
   background-color: var(--block-background-color);
   border-radius: var(--block-border-radius);
   border: var(--block-border-width) var(--block-border-color) solid;
@@ -24,90 +62,101 @@ const styles = `
   display: flex;
   flex-direction: column;
   color: var(--font-color);
-  font-size: var(--font-size);
 }
 
-.mastodon-comment p {
+.misskey-comment p {
   margin-bottom: 0px;
 }
 
-.mastodon-comment .author {
+.misskey-comment .author {
   padding-top:0;
   display:flex;
 }
 
-.mastodon-comment .author a {
+.misskey-comment .author a {
   text-decoration: none;
+  color: var(--font-color); /* Nomi autori chiari */
 }
 
-.mastodon-comment .author .avatar img {
+.misskey-comment .author .avatar img {
   margin-right:1rem;
   min-width:60px;
   border-radius: 5px;
 }
 
-.mastodon-comment .author .details {
+.misskey-comment .author .details {
   display: flex;
   flex-direction: column;
+  line-height: 1.2em;
 }
 
-.mastodon-comment .author .details .name {
+.misskey-comment .author .details .name {
   font-weight: bold;
+  color: #ffffff; /* Nome utente principale in bianco */
 }
 
-.mastodon-comment .author .details .user {
-  color: #5d686f;
+.misskey-comment .author .details .user {
+  color: #aaaaaa; /* Handle utente in grigio chiaro */
   font-size: medium;
 }
 
-.mastodon-comment .author .date {
+.misskey-comment .author .details a {
+    color: inherit; /* Assicura che i link usino il colore del contenitore */
+}
+
+.misskey-comment .author .date {
   margin-left: auto;
   font-size: small;
+  color: #888888; /* Data in grigio tenue */
 }
 
-.mastodon-comment .content {
-  margin: 15px 20px;
+.misskey-comment .content {
+  margin: 15px 0;
+  line-height: 1.5em;
+  color: var(--font-color);
 }
 
-.mastodon-comment .attachments {
+.misskey-comment .author .details a,
+.misskey-comment .content p {
+  margin-bottom: 10px;
+}
+
+.misskey-comment .attachments {
   margin: 0px 10px;
 }
 
-.mastodon-comment .attachments > * {
+.misskey-comment .attachments > * {
   margin: 0px 10px;
 }
 
-.mastodon-comment .attachments img {
+.misskey-comment .attachments img {
   max-width: 100%;
 }
 
-.mastodon-comment .content p:first-child {
-  margin-top:0;
-  margin-bottom:0;
-}
-
-/* Misskey non ha un mapping diretto 1:1 di queste classi, 
-   ma le manteniamo per coerenza. Verranno usati renoteCount e reactions. */
-.mastodon-comment .status > div {
+.misskey-comment .status > div, #misskey-stats > div {
   display: inline-block;
   margin-right: 15px;
 }
 
-.mastodon-comment .status a {
-  color: #5d686f;
+.misskey-comment .status a, #misskey-stats a {
+  color: #888888; /* Colore base delle statistiche */
   text-decoration: none;
 }
 
-.mastodon-comment .status .replies.active a {
-  color: #003eaa;
+.misskey-comment .status .replies.active a, #misskey-stats .replies.active a {
+  color: #61b1ff; /* Blu acceso per le risposte (attive) */
 }
 
-.mastodon-comment .status .reblogs.active a {
-  color: #8c8dff;
+.misskey-comment .status .reblogs.active a, #misskey-stats .reblogs.active a {
+  color: #b28cff; /* Viola acceso per i Renote (attivi) */
 }
 
-.mastodon-comment .status .favourites.active a {
-  color: #ca8f04;
+.misskey-comment .status .favourites.active a, #misskey-stats .favourites.active a {
+  color: #ffc461; /* Giallo/Arancio acceso per le Reazioni (attive) */
+}
+
+#error {
+    color: #ff6161; /* Colore per i messaggi di errore/noscript */
 }
 `;
 
@@ -117,7 +166,7 @@ class MisskeyComments extends HTMLElement {
 
     this.host = this.getAttribute("host");
     this.user = this.getAttribute("user");
-    this.noteId = this.getAttribute("noteId"); // Usiamo "noteId" per chiarezza con Misskey
+    this.noteId = this.getAttribute("noteId"); // Usiamo "noteId" per Misskey
 
     this.commentsLoaded = false;
 
@@ -128,21 +177,22 @@ class MisskeyComments extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-      <h2>Commenti</h2>
+      <div id="misskey-stats"></div>
+      <div id="misskey-title">Commenti</div>
 
       <noscript>
         <div id="error">
-          Please enable JavaScript to view the comments powered by the Fediverse.
+          Per visualizzare i commenti del Fediverso devi abilitare JavaScript.
         </div>
       </noscript>
 
-      <p>Per commentare <a class="link"
-      href="https://${this.host}/notes/${this.noteId}">questo post</a> devi avere un account nel Fediverso.
+      <p>Puoi usare il tuo account del Fediverso (es. Misskey, Mastodon) per rispondere a <a class="link"
+          href="https://${this.host}/notes/${this.noteId}" rel="ugc">questo post</a>.
       </p>
-      <p id="mastodon-comments-list"></p>
+      <ul id="misskey-comments-list"></ul>
     `;
 
-    const comments = document.getElementById("mastodon-comments-list");
+    const comments = document.getElementById("misskey-comments-list");
     const rootStyle = this.getAttribute("style");
     if (rootStyle) {
       comments.setAttribute("style", rootStyle);
@@ -159,14 +209,13 @@ class MisskeyComments extends HTMLElement {
       .replace(/'/g, "&#039;");
   }
 
-  // --- Adattamento Misskey ---
+  // --- Funzioni specifiche Misskey (Adattamento della logica Mastodon) ---
 
   note_active(note, what) {
-    // Gestione semplificata per Misskey
     if (what === "replies") return note.repliesCount > 0 ? "active" : "";
     if (what === "reblogs") return note.renoteCount > 0 ? "active" : "";
     
-    // Per le reactions (favoriti), assumiamo che qualsiasi reazione sia "attiva"
+    // Per le reazioni, controlla se l'oggetto reactions ha voci
     if (what === "favourites" && note.reactions) {
         return Object.keys(note.reactions).length > 0 ? "active" : "";
     }
@@ -177,7 +226,7 @@ class MisskeyComments extends HTMLElement {
     if (what === "replies") return note.repliesCount || "";
     if (what === "reblogs") return note.renoteCount || "";
     
-    // Per i favoriti/reactions, restituiamo il numero totale di reazioni uniche
+    // Per i preferiti/reazioni, somma i conteggi di tutte le reazioni uniche
     if (what === "favourites" && note.reactions) {
         let totalReactions = 0;
         for (const key in note.reactions) {
@@ -188,45 +237,89 @@ class MisskeyComments extends HTMLElement {
     return "";
   }
 
+  note_stats(note) {
+    const noteUrl = `https://${this.host}/notes/${note.id}`;
+    return `
+      <div class="replies ${this.note_active(note, "replies")}">
+        <a href="${
+          noteUrl
+        }" rel="ugc nofollow"><i class="fa fa-reply fa-fw"></i>${this.note_count(
+          note,
+          "replies",
+        )}</a>
+      </div>
+      <div class="reblogs ${this.note_active(note, "reblogs")}">
+        <a href="${
+          noteUrl
+        }" rel="nofollow"><i class="fa fa-retweet fa-fw"></i>${this.note_count(
+          note,
+          "reblogs",
+        )}</a>
+      </div>
+      <div class="favourites ${this.note_active(note, "favourites")}">
+        <a href="${
+          noteUrl
+        }" rel="nofollow"><i class="fa fa-star fa-fw"></i>${this.note_count(
+          note,
+          "favourites",
+        )}</a>
+      </div>
+    `;
+  }
+
   user_account(user) {
     // Misskey fornisce username e host
     return `@${user.username}${user.host ? '@' + user.host : ''}`;
   }
 
-  render_notes(notes, in_reply_to_id, depth) {
-    // Misskey usa 'replyId' al posto di 'in_reply_to_id'
+  render_notes(notes, in_reply_to_id) {
+    // Misskey usa 'replyId'
     var notesToRender = notes
       .filter((note) => note.replyId === in_reply_to_id)
       .sort((a, b) => a.createdAt.localeCompare(b.createdAt));
       
-    notesToRender.forEach((note) => this.render_note(notes, note, depth));
+    notesToRender.forEach((note) => this.render_note(notes, note));
   }
 
-  render_note(notes, note, depth) {
+  render_note(notes, note) {
+    const user = note.user;
+    
     // Misskey usa 'text' per il contenuto
     const content = note.text || ""; 
-    
-    // Su Misskey, il nome è in note.user.name, l'avatar in note.user.avatarUrl
-    const user = note.user;
     
     // Misskey usa 'files' per gli allegati
     const attachmentsHtml = (note.files || [])
       .map((attachment) => {
         if (attachment.type.startsWith("image/")) {
-            return `<a href="${attachment.url}" rel="nofollow"><img src="${
+            return `<a href="${attachment.url}" rel="ugc nofollow"><img src="${
                 attachment.url
-            }" alt="Allegato immagine" /></a>`;
+            }" alt="${this.escapeHtml(attachment.name)}" loading="lazy" /></a>`;
         } else if (attachment.type.startsWith("video/")) {
-            return `<video controls><source src="${attachment.url}" type="${attachment.type}"></video>`;
+            return `<video controls preload="none"><source src="${attachment.url}" type="${attachment.type}"></video>`;
         } else if (attachment.type.startsWith("audio/")) {
             return `<audio controls><source src="${attachment.url}" type="${attachment.type}"></audio>`;
         } else {
-            return `<a href="${attachment.url}" rel="nofollow">${attachment.type}</a>`;
+            return `<a href="${attachment.url}" rel="ugc nofollow">${attachment.name}</a>`;
         }
       })
       .join("");
-    
-    const misskeyComment = `<div class="mastodon-comment" style="margin-left: calc(var(--comment-indent) * ${depth})">
+
+
+    const formatDate = (dateString) => {
+      // Misskey usa createdAt. La formattazione è localizzata in italiano.
+      return new Date(dateString).toLocaleString('it-IT', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false,
+        formatMatcher: 'basic'
+      });
+    }
+
+    const misskeyComment = `
+      <article class="misskey-comment">
         <div class="author">
           <div class="avatar">
             <img src="${this.escapeHtml(
@@ -242,83 +335,91 @@ class MisskeyComments extends HTMLElement {
             }" rel="nofollow">${this.user_account(user)}</a>
           </div>
           <a class="date" href="${
-            note.url || `https://${this.host}/notes/${note.id}`
-          }" rel="nofollow">${note.createdAt.substr(
-            0,
-            10,
-          )} ${note.createdAt.substr(11, 8)}</a>
+            `https://${this.host}/notes/${note.id}`
+          }" rel="nofollow">
+              <time datetime="${note.createdAt}">
+                ${formatDate(note.createdAt)}
+              </time>
+          </a>
         </div>
         <div class="content">${content}</div>
         <div class="attachments">${attachmentsHtml}</div>
         <div class="status">
-          <div class="replies ${this.note_active(note, "replies")}">
-            <a href="${
-              note.url || `https://${this.host}/notes/${note.id}`
-            }" rel="nofollow"><i class="fa fa-reply fa-fw"></i>${this.note_count(
-              note,
-              "replies",
-            )}</a>
-          </div>
-          <div class="reblogs ${this.note_active(note, "reblogs")}">
-            <a href="${
-              note.url || `https://${this.host}/notes/${note.id}`
-            }" rel="nofollow"><i class="fa fa-retweet fa-fw"></i>${this.note_count(
-              note,
-              "reblogs",
-            )}</a>
-          </div>
-          <div class="favourites ${this.note_active(note, "favourites")}">
-            <a href="${
-              note.url || `https://${this.host}/notes/${note.id}`
-            }" rel="nofollow"><i class="fa fa-star fa-fw"></i>${this.note_count(
-              note,
-              "favourites",
-            )}</a>
-          </div>
+          ${this.note_stats(note)}
         </div>
-      </div>`;
+      </article>
+    `;
 
-    var div = document.createElement("div");
-    div.innerHTML =
+    var li = document.createElement("li");
+    li.setAttribute("id", note.id)
+    li.innerHTML =
       typeof DOMPurify !== "undefined"
         ? DOMPurify.sanitize(misskeyComment.trim())
         : misskeyComment.trim();
-    document
-      .getElementById("mastodon-comments-list")
-      .appendChild(div.firstChild);
+        
+    // Logica per inserire il commento nell'elenco o nell'elenco nidificato
+    if (note.replyId === this.noteId) {
+        document.getElementById("misskey-comments-list").appendChild(li);
+    } else {
+        const parentNote = notes.find(n => n.id === note.replyId);
+        if (parentNote) {
+            let parentElement = document.getElementById(note.replyId);
+            if (parentElement) {
+                let ul = parentElement.querySelector('ul');
+                if (!ul) {
+                    ul = document.createElement('ul');
+                    parentElement.appendChild(ul);
+                }
+                ul.appendChild(li);
+            }
+        }
+    }
 
-    this.render_notes(notes, note.id, depth + 1);
+    this.render_notes(notes, note.id);
   }
 
   loadComments() {
     if (this.commentsLoaded) return;
 
-    document.getElementById("mastodon-comments-list").innerHTML =
-      "Loading comments from the Fediverse (Misskey)...";
+    document.getElementById("misskey-comments-list").innerHTML =
+      "Caricamento dei commenti dal Fediverso (Misskey)...";
 
     let _this = this;
-
-    // Richiesta POST all'API Misskey
-    fetch(
-      "https://" + this.host + "/api/notes/replies", {
+    
+    // 1. Recupera la nota principale per le sue statistiche
+    fetch(`https://${this.host}/api/notes/show`, {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ noteId: this.noteId })
+    })
+    .then((response) => response.json())
+    .then((note) => {
+        if (note && note.id) {
+            document.getElementById("misskey-stats").innerHTML =
+                this.note_stats(note);
+        }
+    })
+    .catch(error => console.error("Errore nel caricamento delle statistiche Misskey:", error));
+
+
+    // 2. Recupera le risposte (il contesto dei commenti)
+    fetch(
+      `https://${this.host}/api/notes/replies`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
             noteId: this.noteId,
-            limit: 100 // Limite di commenti da caricare
+            limit: 100 
         })
     })
       .then((response) => response.json())
       .then((data) => {
-        // L'API Misskey restituisce direttamente un array di note (i commenti)
+        // Misskey restituisce direttamente un array di note (i commenti)
         if (Array.isArray(data) && data.length > 0) {
-          document.getElementById("mastodon-comments-list").innerHTML = "";
-          // Chiamiamo render_notes con la lista dei commenti e l'ID della nota radice
-          _this.render_notes(data, _this.noteId, 0); 
+          document.getElementById("misskey-comments-list").innerHTML = "";
+          _this.render_notes(data, _this.noteId);
         } else {
-          document.getElementById("mastodon-comments-list").innerHTML =
+          document.getElementById("misskey-comments-list").innerHTML =
             "<p>Nessun commento trovato</p>";
         }
 
@@ -326,12 +427,11 @@ class MisskeyComments extends HTMLElement {
       })
       .catch(error => {
           console.error("Errore nel caricamento dei commenti Misskey:", error);
-          document.getElementById("mastodon-comments-list").innerHTML =
-            "<p>Errore nel caricamento dei commenti Misskey.</p>";
+          document.getElementById("misskey-comments-list").innerHTML =
+            "<p>Errore nel caricamento dei commenti da Misskey.</p>";
       });
   }
 
-  // Funzione di utilità per caricare solo quando visibile
   respondToVisibility(element, callback) {
     var options = {
       root: null,
@@ -349,5 +449,4 @@ class MisskeyComments extends HTMLElement {
   }
 }
 
-// Definisci il nuovo elemento custom
 customElements.define("misskey-comments", MisskeyComments);
